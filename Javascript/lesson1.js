82c56cb636cd81bf76e0a31ec7878a866ac32939
hackerAbdul <abdulkadir.q12@gmail.com>
2023-07-15 14:14:06 +0100

    starting javascript

---
+++ Javascript/lesson1.js
@@ -0,0 +1,22 @@
+//this is a single line comment
+
+/*
+this 
+will
+be 
+a
+multi-line
+comment*/
+
+console.log("Hello you have executed the lesson 1 file")
+
+console.log("hello world!")
+
+var greeting = 'hello'
+var myName = "abdulkadir"
+
+
+
+
+console.log(greeting+" "+myName+1);
+console.log(8484+54984+45684);
